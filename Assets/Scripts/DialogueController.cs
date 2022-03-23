504ae616a10f2e2e94f99277c42ddd45fd085532
Vergervan <krazy.vovan@mail.ru>
2022-03-23 15:48:21 +0300

    Improve code

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -30,10 +30,7 @@ public class DialogueController : MonoBehaviour
                 if (route.Switchable && !route.To.HasAvailableRoutes)
                 {
                     route.TurnOff();
-                    if (!route.Parent.HasAvailableRoutes)
-                    {
-                        route.Parent.ParentRoute.TurnOff();
-                    }
+                    CheckParentRoutes(route);
                 }
                 if (route.HasTriggers)
                 {
@@ -55,12 +52,39 @@ public class DialogueController : MonoBehaviour
             }
         }
     }
+    private void CheckParentRoutes(Route route)
+    {
+        if (Equals(route.Parent, null) || Equals(route.Parent.ParentRoute, null)) return;
+        foreach(var item in route.Parent.ParentRoute.Parent.Routes)
+        {
+            if(item.To.Name == route.Parent.Name)
+            {
+                if (!item.To.HasAvailableRoutes)
+                {
+                    item.TurnOff();
+                    CheckParentRoutes(item);
+                    break;
+                }
+            }
+        }
+    }
     public void SetRoutes(NPCBehaviour npc, IEnumerable<Route> routes)
     {
         currentSelection = 0;
         currentNpc = npc;
         choicesRoutes.Clear();
-        choicesRoutes.AddRange(routes.Where(route => route.Available && route.ConditionsMet && (!route.To.IsDialog || route.To.HasAvailableRoutes)));
+        
+        foreach(var route in routes)
+        {
+            if (route.Parent != null)
+                Debug.Log(route.Parent.Name);
+            //Debug.Log($"Route {route.From.Name} | Available {route.Available} {route.ConditionsMet} \n{route.To.IsDialog} {route.To.HasAvailableRoutes}");
+            //Debug.Log(route.To.RoutesCount + " " + route.To.AvailableRoutesCount);
+            if (route.Available && route.ConditionsMet && (!route.To.IsDialog || route.To.HasAvailableRoutes))
+                choicesRoutes.Add(route);
+        }
+
+        //choicesRoutes.AddRange(routes.Where(route => route.Available && route.ConditionsMet && (!route.To.IsDialog || route.To.HasAvailableRoutes)));
         ClearContent();
         BuildDialogWindowItems();
         //window.ToggleWindow();
