dc449d5f029e0bafee34b0efdc9a14b17707722a
Vergervan <krazy.vovan@mail.ru>
2022-07-10 00:17:42 +0300

    improve code

--- Assets/Scripts/UI/StretchWindow.cs
+++ Assets/Scripts/UI/StretchWindow.cs
@@ -6,25 +6,16 @@ public class StretchWindow : MonoBehaviour, IDragHandler
     [SerializeField] private GameWindow window;
     [SerializeField] private Canvas canvas;
     private readonly Vector2 minSize = new Vector2(200, 200);
+    private float allowedProx = 1f;
 
-    private void Awake()
-    {
-        
-    }
     public void OnDrag(PointerEventData eventData)
     {
         if (eventData.button == PointerEventData.InputButton.Left)
         {
-            if (eventData.position.y < 1f) return;
+            if (eventData.position.y < allowedProx || eventData.position.x > (canvas.pixelRect.width - allowedProx)) return;
             Vector2 newSize = window.Rect.sizeDelta + (new Vector2(eventData.delta.x, -eventData.delta.y) / canvas.scaleFactor);
-            if(newSize.x < minSize.x)
-            {
-                newSize.x = minSize.x;
-            }
-            if(newSize.y < minSize.y)
-            {
-                newSize.y = minSize.y;
-            }
+            newSize.x = Mathf.Max(minSize.x, newSize.x);
+            newSize.y = Mathf.Max(minSize.y, newSize.y);
             window.Rect.sizeDelta = newSize;
             window.Bar.UpdateBorders();
         }
