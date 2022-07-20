93f3c0a55f006cd5a25b0d8cdb17f1841c0a6790
Vergervan <krazy.vovan@mail.ru>
2022-07-21 00:20:12 +0300

    add save game opportunity

---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    private SavedDataHandler dataHandler;
+    void Awake()
+    {
+        dataHandler = SavedDataHandler.GetInstance();
+    }
+
+    public void SaveGame()
+    {
+        dataHandler.SaveData("test_save");
+    }
+}
--- Assets/Scripts/SavedDataHandler.cs
+++ Assets/Scripts/SavedDataHandler.cs
@@ -5,6 +5,8 @@ using System;
 using System.Linq;
 
 using Debug = UnityEngine.Debug;
+using System.Text;
+using System.IO;
 
 public interface ILoadSavedData
 {
@@ -17,6 +19,7 @@ public class SavedDataHandler
 {
     private static SavedDataHandler _instance = null;
     private List<ILoadSavedData> _data;
+    private StringBuilder _newFile;
 
     private SavedDataHandler()
     {
@@ -26,26 +29,6 @@ public class SavedDataHandler
     public void Subscribe(ILoadSavedData data)
     {
         _data.Add(data);
-        Type type = data.GetType();
-        foreach (var field in type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
-        {
-            SaveFieldAttribute fieldAttribute = (SaveFieldAttribute)Attribute.GetCustomAttribute(field, typeof(SaveFieldAttribute));
-            if(fieldAttribute != null)
-            {
-                var val = field.GetValue(data);
-                if (val is IEnumerable)
-                {
-                    Debug.Log($"{field.Name} is collection");
-                    int counter = 0;
-                    foreach(var item in val as IEnumerable)
-                    {
-                        counter++;
-                        Debug.Log($"{counter} {item}");
-                    }
-                    Debug.Log("Count: " + counter);
-                }
-            }
-        }
     }
     public void Unsubscribe(ILoadSavedData data)
     {
@@ -62,8 +45,67 @@ public class SavedDataHandler
     {
 
     }
-    public void SaveData(string path)
+    public void SaveData(string saveName)
     {
-        
+        _newFile = new StringBuilder();
+        StringBuilder iterBuilder = new StringBuilder();
+        lock (_data)
+        {
+            foreach (var item in _data)
+            {
+                Type type = item.GetType();
+                foreach (var field in type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+                {
+                    iterBuilder.Clear();
+                    SaveFieldAttribute fieldAttribute = (SaveFieldAttribute)Attribute.GetCustomAttribute(field, typeof(SaveFieldAttribute));
+                    if (fieldAttribute != null)
+                    {
+                        var fieldValue = field.GetValue(item);
+                        iterBuilder.Append($"{field.Name}=");
+                        if (fieldValue is IEnumerable)
+                        {
+                            IEnumerable arr = fieldValue as IEnumerable;
+                            iterBuilder.Append('[');
+                            int counter = 0;
+                            foreach (var it in arr)
+                            {
+                                if(counter != 0)
+                                {
+                                    iterBuilder.Append(',');
+                                }
+                                iterBuilder.Append(it);
+                                counter++;
+                            }
+                            if (counter == 0) continue;
+                            iterBuilder.Append("]\n");
+                        }
+                        else
+                        {
+                            iterBuilder.Append($"{field.Name}={fieldValue}\n");
+                        }
+                        _newFile.Append(iterBuilder.ToString());
+                    }
+                }
+            }
+        }
+        CreateFile(saveName);
+    }
+    private void CreateFile(string saveName)
+    {
+        saveName += ".gsv";
+#if UNITY_STANDALONE && !UNITY_EDITOR
+        if (!Directory.Exists("saves"))
+        {
+            Directory.CreateDirectory("saves");
+        }
+        saveName = "./saves/" + saveName;
+#endif
+        using (FileStream fs = new FileStream(saveName, FileMode.OpenOrCreate))
+        {
+            byte[] buf = Encoding.UTF8.GetBytes(_newFile.ToString());
+            fs.Write(buf, 0, buf.Length);
+            fs.Flush();
+        }
+        _newFile.Clear();
     }
 }
